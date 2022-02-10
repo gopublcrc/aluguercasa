4ae15320129d73a2e257714fffdf49b09da8e29b
gopublcrc <99414557+gopublcrc@users.noreply.github.com>
2022-02-10 16:06:53 +0100

    Create casa.php

---
+++ casa.php
@@ -0,0 +1,13 @@
+echo "# aluguercasa" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gopublcrc/aluguercasa.git
+git push -u origin mainecho "# aluguercasa" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gopublcrc/aluguercasa.git
+git push -u origin main
